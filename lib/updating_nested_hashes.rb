1dcd58d6b650fc3623447e24b20f394ff4e63c06
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-27 04:02:41 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -46,9 +46,7 @@ def add_characters
          ]
       }
    }
-
-   # Write code below that does the following programmatically:
-   epic_tragedy[:additional_characters] = []
+   epic_tragedy[:additional_characters] = Array.new
     :additional_characters => [
       {name: "Prince Escalus"},
       {name: "Apothecary"}
